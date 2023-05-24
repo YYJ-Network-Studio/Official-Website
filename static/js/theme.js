1ca603527e3dbfe3d088df18f602aceb88104103
YYJ <68520446+YYJ-TW@users.noreply.github.com>
2023-05-24 17:25:06 +0800

    Adjust theme colors

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,6 +1,7 @@
-const button = document.getElementById("toggle");
-const body = document.querySelector("body");
-const container = document.querySelector(".container");
+const button = document.getElementById('toggle');
+const body = document.querySelector('body');
+const container = document.querySelector('.container');
+const resumeTitles = document.querySelectorAll('.resume-title');
 
 window.onload = function() {
     const theme = localStorage.getItem('theme');
@@ -8,17 +9,33 @@ window.onload = function() {
         button.classList.add('dark');
         container.classList.add('dark');
         body.classList.add('dark');
+        resumeTime.classList.add('dark')
+        toggleResumeTitlesClass('add', 'dark');
     }
-}
+};
 
 button.onclick = function() {
     button.classList.toggle('dark');
-    container.classList.toggle('dark');
     body.classList.toggle('dark');
+    container.classList.toggle('dark');
+    toggleResumeTitles('toggle', 'dark');
     
     if (localStorage.getItem('theme') === 'dark') {
         localStorage.setItem('theme', 'light');
     } else {
         localStorage.setItem('theme', 'dark');
+    }
+};
+
+function toggleResumeTitles(action, className) {
+    for (let i = 0; i < resumeTitles.length; i++) {
+        const resumeTitle = resumeTitles[i];
+        if (action === 'add') {
+            resumeTitle.classList.add(className);
+        } else if (action === 'remove') {
+            resumeTitle.classList.remove(className);
+        } else if (action === 'toggle') {
+            resumeTitle.classList.toggle(className);
+        }
     }
 }
